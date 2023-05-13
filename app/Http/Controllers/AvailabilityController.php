133a812e42ec9ad74ca4236c9d2ffd5cffc13255
Odiljon <odilorg@gmail.com>
2023-05-13 23:01:18 +0500

    need to itirate and picl ones with availabilities

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -3,13 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class AvailabilityController extends Controller
 {
     
-    public function availability()
+    public function availability(Request $request)
     {
-        dd('availability');
+      
+        $arrival_date = $request->input('arrive');
+        $departure_date = $request->input('departure');
+        $num_adults = $request->input('adults');
+        $num_children = $request->input('children');
+      
+        $response = Http::post('https://api.beds24.com/json/getAvailabilities', [
+            'checkIn' =>  $arrival_date,
+            'checkOut' => $departure_date,
+            'propId' => '172793',
+            'numAdult' => $num_adults,
+            'numChild' => $num_children
+        ]);
+        //$data = json_decode($response->object());
+
+
+$ar =$response->collect();
+$filtered = $ar->flatten();
+foreach ($filtered as $value) {
+    if () {
+        # code...
+    }
+}
+
+
+        dd($filtered);
     }
     
     
